8fd9d49af3eddf846563c9f41aeef7ed6c109811
Amos Boldor <amosboldor@gmail.com>
2017-01-23 18:26:31 -0800

    add scafold of hash_table class

--- src/tests/test_hash_table.py
+++ src/tests/test_hash_table.py
@@ -1,1 +1,23 @@
 """Tests for Hash Table ."""
+
+
+class HashTable(object):
+    """Class HashTable."""
+
+    def __init__(self, num):
+        """Initialize with number of slots to be in table."""
+        self.table = []
+        for x in range(0, num):
+            self.table.append([])
+
+    def _hash(self, key):
+        """Sash the key provided and return."""
+        pass
+
+    def set(self, key, val):
+        """Store the given val using the given key."""
+        pass
+
+    def get(self, key):
+        """Return the value stored with the given key."""
+        pass
