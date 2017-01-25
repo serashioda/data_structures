448a0913ee653c8a74d59e292fac9dceb525702b
Amos Boldor <amosboldor@gmail.com>
2017-01-25 14:08:52 -0800

    add trie class with __init__ insert and contains methods also added test fixture

--- src/tests/test_trie.py
+++ src/tests/test_trie.py
@@ -1,1 +1,22 @@
 """Test for Trie implementation."""
+
+import pytest
+import random
+
+
+@pytest.fixture
+def full_trie():
+    """Fixture to return full trie."""
+    from trie import Trie
+    trie = Trie()
+    all_words = []
+    with open('/usr/share/dict/words') as a:
+        for line in a:
+            if "'" not in line:
+                all_words.append(line.strip('\n'))
+    words = []
+    for x in range(1, 200):
+        word = random.choice(all_words)
+        trie.insert(word)
+        words.append(word)
+    return trie, words
--- src/trie.py
+++ src/trie.py
@@ -1,1 +1,29 @@
 """Trie implementation."""
+
+
+class Trie(object):
+    """Trie Class."""
+
+    def __init__(self):
+        """Initialize a root for Trei."""
+        self.root = {}
+        self._size = 0
+
+    def insert(self, string):
+        """Insert string into trie."""
+        current = self.root
+        for char in string.lower():
+            current = current.setdefault(char, {})
+        current.setdefault("$")
+        self._size += 1
+
+    def contains(self, string):
+        """Return True if string in the trie."""
+        current = self.root
+        for char in string.lower():
+            if char not in current:
+                return False
+            current = current[char]
+        if "$" in current:
+            return True
+        return False
