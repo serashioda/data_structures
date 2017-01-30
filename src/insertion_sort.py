c09505705369eeab9944cd45de34d5b404d12afe
Your Name <you@example.com>
2017-01-30 14:40:19 -0800

    timeit complete

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -6,17 +6,24 @@ def insertion_sort(numbers):
     def find_op(idx, lst):
         return len(lst) - idx - 1
     for idx, num in enumerate(numbers):
-        print(numbers)
         num_sorted = numbers[:idx]
         num_sorted = num_sorted[::-1]
-        print('Reversed rest of numbers: ', num_sorted)
         for idx_2, num_sort in enumerate(num_sorted):
-            print("Num to sort: ", num_sort)
             if num_sort > num:
-                print(numbers[idx], num_sort)
                 numbers[idx] = num_sort
-                print(numbers[find_op(idx_2, num_sorted)], num)
                 numbers[find_op(idx_2, num_sorted)] = num
                 idx = find_op(idx_2, num_sorted)
-                print('Replace', numbers)
     return numbers
+
+if __name__ == '__main__':  # pragma: no cover
+    import timeit
+    import random
+
+    a = [random.randint(0, 21) for x in range(0, 21)]
+
+    insertion = timeit.timeit(
+        stmt='insertion_sort(' + str(a) + ')',
+        setup='from __main__ import insertion_sort',
+        number=1000,
+    )
+    print('The average time for insertion_sort after 1000 times ran:' + str(insertion))
