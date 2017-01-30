2fe5db6b5b35fe1be1e2284cb5796589c23b619a
Amos Boldor <amosboldor@gmail.com>
2017-01-30 13:14:40 -0800

    a start of insertion sort

---
+++ src/insertion_sort.py
@@ -0,0 +1,21 @@
+"""Implementation of insertion sort."""
+
+
+def insertion_sort(numbers):
+    """."""
+    def find_op(idx, lst):
+        return len(lst) - idx - 1
+    for idx, num in enumerate(numbers):
+        print(numbers)
+        num_sorted = numbers[:idx]
+        num_sorted = num_sorted[::-1]
+        print('Reversed rest of numbers: ', num_sorted)
+        for idx_2, num_sort in enumerate(num_sorted):
+            print("Num to sort: ", num_sort)
+            if num_sort > num:
+                print(numbers[idx], num_sort)
+                numbers[idx] = num_sort
+                print(numbers[find_op(idx_2, num_sorted)], num)
+                numbers[find_op(idx_2, num_sorted)] = num
+                print('Replace', numbers)
+    return numbers
