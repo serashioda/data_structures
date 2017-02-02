2d87417f6742c23bd35b3119280d097cf0519286
Amos Boldor <amosboldor@gmail.com>
2017-02-02 15:01:34 -0800

    small fix

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -5,14 +5,13 @@ def radix_sort(unsorted_lst):
     """Radix_sort."""
     max_num = max(unsorted_lst)
     passes = len(str(max_num))
-    padded_lst = []
     buckets = [[] for x in range(0, 10)]
     while passes:
         for x in unsorted_lst:
             x = str(x).zfill(passes)
             buckets[int((x[passes - 1]))].append(x)
 
-        unsorted_lst = put_back(bucket)
+        unsorted_lst = put_back(buckets)
         buckets = [[] for x in range(0, 10)]
         passes -= 1
     return [int(x) for x in unsorted_lst]
