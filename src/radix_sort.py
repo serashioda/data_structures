1fb460e7a45761db36ad019465657d3901cbcb69
Your Name <you@example.com>
2017-02-02 14:59:37 -0800

    radix_sort added. adding tests

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -11,3 +11,16 @@ def radix_sort(unsorted_lst):
         for x in unsorted_lst:
             x = str(x).zfill(passes)
             buckets[int((x[passes - 1]))].append(x)
+
+        unsorted_lst = put_back(bucket)
+        buckets = [[] for x in range(0, 10)]
+        passes -= 1
+    return [int(x) for x in unsorted_lst]
+
+
+def put_back(buckets):
+    """Return buckets."""
+    s = []
+    for bucket in buckets:
+        s.extend(bucket)
+    return s
