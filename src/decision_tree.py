343100301ac18a5bb7cfea1eac777ebd63295df5
Amos Boldor <amosboldor@gmail.com>
2017-02-10 10:31:45 -0800

    some code to determine where to split

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -4,13 +4,14 @@
 class TreeNode(object):
     """Tree Node for decision trees."""
 
-    def __init__(self, column=None, split=None, left=None, right=None, data_idx=None):
-        """Initialize variables for tree node."""
-        self.column = column
-        self.split = split
+    def __init__(self, data, split_value, split_gini, split_col, left=None, right=None, parent=None):
+        """Initialize a node object for a decision tree classifier."""
         self.left = left
         self.right = right
-        self.data_idx = data_idx
+        self.data = data
+        self.split_value = split_value
+        self.split_gini = split_gini
+        self.split_col = split_col
 
 
 class DecisionTree(object):
@@ -22,38 +23,44 @@ class DecisionTree(object):
         self.min_leaf_size = min_leaf_size
         self.root = None
 
-    def fit(self, input_df, labels):
-        """Method for fittting a new tree."""
-        self.root = TreeNode(data_idx=input_df)
-        # start spliting
-        self._split(self.root)
+    def fit(self, data):
+        """Create a tree to fit the data."""
+        split_col, split_value, split_gini, split_groups = self._get_split(data)
+        new_node = TreeNode(data, split_value, split_gini, split_col)
+        if not self.root:
+            self.root = new_node
 
-    def predict(self, columns):
-        """Given some data seperated into columns.
+    def _test_split(self, index, value, dataset):
+        """Split a dataset based on an attribute and an attribute value."""
+        left, right = list(), list()
+        for row in dataset:
+            if row[index] < value:
+                left.append(row)
+            else:
+                right.append(row)
+        return left, right
 
-        return new labels for data given.
-        """
-        pass
+    def _gini_index(self, groups, class_values):
+        """Calculate the Gini index for a split dataset."""
+        gini = 0.0
+        for class_value in class_values:
+            for group in groups:
+                size = len(group)
+                if size == 0:
+                    continue
+                proportion = [row[-1] for row in group].count(class_value) / float(size)
+                gini += (proportion * (1.0 - proportion))
+        return gini
 
-    def _split(self, node):
-        """Given some node containing data.
-
-        find best column to split on and assign split point and child nodes.
-        """
-        column, split_point = self._best_split_point_algorithm()
-#         put column and split_point
-#         if result of spliting produces nodes with atleast one values:
-#             node.left = TreeNode()
-#             node.right = TreeNode()
-#         if left has one value or right is purely one label:
-#             end left
-#         if right has one value or right is purely one label:
-#             end right
-        pass
-
-    def gini(self, p):
-        """Given a dataset return same data set giniafide."""
-        return (p) * (1 - (p)) + (1 - p) * (1 - (1 - p))
-
-    def _best_split_point_algorithm():
-        pass
+    def _get_split(self, dataset):
+        """Select the best split point for a dataset."""
+        class_values = list(set(row[-1] for row in dataset))
+        b_index, b_value, b_score, b_groups = 999, 999, 999, None
+        for index in range(len(dataset[0]) - 1):
+            for row in dataset:
+                groups = self._test_split(index, row[index], dataset)
+                gini = self._gini_index(groups, class_values)
+                print('X%d < %.3f Gini=%.3f' % ((index + 1), row[index], gini))
+                if gini < b_score:
+                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+        return b_index, b_value, b_score, b_groups
