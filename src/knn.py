85cd4bc4d432c1062f94ed39ef0978df2794f138
Amos Boldor <amosboldor@gmail.com>
2017-02-19 13:57:35 -0800

    add K-Nearest Neighbors class with __init__ function

---
+++ src/knn.py
@@ -0,0 +1,12 @@
+"""Implemention of K Nearest Neighbour Classifier."""
+
+
+class Knn(object):
+    """K Nearest Neigbour Classifier."""
+
+    def __init__(self, dataset, k=5):
+        """Initialize classifier with dateset and k integer."""
+        if k > len(dataset) or k < 0 or type(k) is not int:
+            raise ValueError("k must be an integer between 1 and the length of the dataset")
+        self.dataset = dataset
+        self.k = 5
